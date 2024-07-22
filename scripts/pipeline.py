301728d5e69672684151185a2c3330d7b5013c55
Maxwell Gao <maxwell-gao314@outlook.com>
2024-07-23 00:51:29 +0800

    Modified pipeline, remove header lines

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -1,21 +1,3 @@
-# This code has been migrated to diffusers but can be run locally with
-# pipe = DiffusionPipeline.from_pretrained("teticio/audio-diffusion-256", custom_pipeline="audio-diffusion/audiodiffusion/pipeline_audio_diffusion.py")
-
-# Copyright 2022 The HuggingFace Team. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
 from math import acos, sin
 from typing import List, Tuple, Union
 
@@ -33,7 +15,7 @@ from diffusers import (
 from diffusers.utils import BaseOutput
 from PIL import Image
 
-from .mel import Mel
+from mel import Mel
 
 class DiffusionPipeline(DiffusionPipeline):
     """
